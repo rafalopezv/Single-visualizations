fcfe33d737d2482577e515050b3742f80882b1a3
Rafael <lopezvalverde.rafael@gmail.com>
2022-09-05 22:25:22 -0500

    cleaning code ready

--- joyPlotCovid/code/cleaning.R
+++ joyPlotCovid/code/cleaning.R
@@ -2,24 +2,19 @@
 # about: cleaning covid data
 # sobre: limpieza de bases para gráfico
 #----------------------------------------
-
 library(tidyverse)
+library(magrittr)
 
-#-------------------------
-# limpieza datos globales
-#-------------------------
-
-# serie de tiempo
+# getting covid dataa/jalando datos del covid
 url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 url_m <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-
-# descarga y limpieza
 df <- read_csv(url) %>% mutate(base = "confirmados")
 df_m <- read_csv(url_m) %>% mutate(base = "fallecidos")
 
 df <- bind_rows(df, df_m) 
 rm(df_m, url, url_m)
 
+# cleaning/limpieza
 df %<>% 
   rename(pais_region = `Country/Region`) %>% 
   dplyr::select(-matches("Lat|Long")) %>% 
@@ -30,12 +25,10 @@ df %<>%
   )) %>% 
   dplyr::select(-`Province/State`) %>% 
   gather(fecha, casos_acumulados, -pais_region, -base) 
-  
+
 df$fecha %<>% as.Date(., format = "%m/%d/%y")
 
-# estandarización desde pacientes 0 e individualización de países en formato lista
 df %>% 
-  # filter(casos_acumulados != 0) %>% 
   group_by(pais_region, base, fecha) %>% 
   summarise_all(sum) %>% 
   ungroup() %>% 
@@ -47,7 +40,7 @@ df %>%
   bind_rows() %>% 
   arrange(base, pais_region, fecha) -> df
 
-# añadir variable de número de semanas: extensión de un año 
+# add weeks/añaadir semanas
 tibble(
   semana = rep((1:200), 7)
 ) %>% 
@@ -59,10 +52,11 @@ rm(temp)
 
 # descarga de población por país: fuente Banco Mundial
 poblacion <- read_csv("input/API_SP.POP.TOTL_DS2_en_csv_v2_1120881/API_SP.POP.TOTL_DS2_en_csv_v2_1120881.csv", skip = 3)
+poblacion <- WDI::WDI(indicator = "SP.POP.TOTL", start = 2018, end = 2018)
 poblacion %<>% 
-  dplyr::select(pais_region = `Country Name`, poblacion = `2018`) 
+  dplyr::select(pais_region = country, poblacion = `SP.POP.TOTL`) 
 
-# compatibilidad d datos población con base JHU
+# make population and covid data compatible/compatibilizar datos de òblación y del COVID
 (df$pais_region %>% unique)[!(df$pais_region %>% unique) %in%  (poblacion$pais_region %>% unique)]
 
 poblacion$pais_region %<>% gsub("Bahamas\\, The", "Bahamas", .)
@@ -88,9 +82,8 @@ poblacion$pais_region %<>% gsub("Congo\\, Dem\\. Rep\\.", "Congo (Kinshasa)", .)
 poblacion$pais_region %<>% gsub("Congo\\, Rep\\.", "Congo (Brazzaville)", .)
 
 
-# Población de Taiwán no está reportada en China: https://databank.worldbank.org/reports.aspx?source=2&type=metadata&series=SP.POP.TOTL
+# Taaiwan population not reportes aas part of China/Población de Taiwán no está reportada en China: https://databank.worldbank.org/reports.aspx?source=2&type=metadata&series=SP.POP.TOTL
 
-# filtro la base de poblacion respecto a la data de JHU
 poblacion %>%  
   filter(pais_region %in% (df %>% pull(pais_region) %>% unique)) %>% 
   dplyr::select(pais_region, poblacion) -> poblacion
@@ -99,18 +92,11 @@ df %<>%
   merge(., poblacion, all.x = T) %>% 
   arrange(base, pais_region, fecha) 
 
-# completar poblaciones manualmente
-df %>% 
-  filter(is.na(poblacion)) %>% 
-  pull(pais_region) %>% 
-  unique
-
-
-df[df$pais_region == "Eritrea", "poblacion"] <- 6081196 # fuente cia.gov, año 2020
-df[df$pais_region == "Taiwan*", "poblacion"] <- 23603049 # fuente cia.gov, año 2020
-df[df$pais_region == "Holy See", "poblacion"] <- 1000 # fuente cia.gov, año 2019
-df[df$pais_region == "Western Sahara", "poblacion"] <- 652271 # fuente cia.gov, año 2020
-
+# complete population manually :( /completar poblaciones manualmente
+df[df$pais_region == "Eritrea", "poblacion"] <- 6081196 # source: cia.gov/fuente cia.gov
+df[df$pais_region == "Taiwan*", "poblacion"] <- 23603049 # source: cia.gov/fuente cia.gov
+df[df$pais_region == "Holy See", "poblacion"] <- 1000 # source: cia.gov/fuente cia.gov
+df[df$pais_region == "Western Sahara", "poblacion"] <- 652271 # source: cia.gov/fuente cia.gov
 
 (df$pais_region %>% unique)[!(df$pais_region %>% unique) %in%  (poblacion$pais_region %>% unique)]
 rm(poblacion)
@@ -119,7 +105,7 @@ rm(poblacion)
 df %<>%  
   filter(!is.na(poblacion))
 
-# dividir entre países/semanas
+# get data weekly/dividir entre países/semanas
 df %>% 
   group_split(base, pais_region) %>% 
   map(., ~arrange(., fecha)) %>% 
@@ -129,5 +115,5 @@ df %>%
   bind_rows() %>% 
   filter(!is.na(incidencia)) -> df_mundo 
 
-
+rm(df, poblacion)
 
